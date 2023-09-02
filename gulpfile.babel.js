9361048835124d5b3f5944b5991891582393754e
Mavi Onogomuho <princetunes@gmail.com>
2023-09-02 18:18:35 +0100

    setup gulp processing for images and files

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -5,6 +5,8 @@ import gulpSass from 'gulp-sass';
 import cleanCss from 'gulp-clean-css';
 import gulpIf from 'gulp-if';
 import sourcemaps from 'gulp-sourcemaps';
+import imagemin from 'gulp-imagemin';
+import del from 'del';
 
 
 const PRODUCTION = yargs.argv.prod;
@@ -14,6 +16,14 @@ const paths = {
     styles: {
         src: ['src/assets/scss/bundle.scss', 'src/assets/scss/admin-bundle.scss'],
         dest: 'dist/assets/css'
+    },
+    images: {
+        src: 'src/assets/images/**/*.{png,jpg,jpeg,svg,gif}',
+        dest: 'dist/assets/images',
+    },
+    other: {
+        src: ['src/assets/**/*', '!src/assets/{images,js,scss}', '!src/assets/{images,js,scss}/**/*'],
+        dest: 'dist/assets',
     }
 };
 
@@ -26,6 +36,26 @@ export const styles = () => {
         .pipe(gulp.dest(paths.styles.dest));
 }
 
+export const images = () => {
+    return gulp.src(paths.images.src)
+        .pipe(gulpIf(PRODUCTION, imagemin()))
+        .pipe(gulp.dest(paths.images.dest))
+}
+
+export const copy = () => {
+    return gulp.src(paths.other.src)
+        .pipe(gulp.dest(paths.other.dest))
+}
+
+export const clean = () => del(['dist']);
+
 export const watch = () => {
     gulp.watch('src/assets/scss/**/*.scss', styles);
+    gulp.watch(paths.images.src, images);
+    gulp.watch(paths.other.src, copy);
 }
+
+export const dev = gulp.series(clean, gulp.parallel(styles, images, copy), watch);
+export const build = gulp.series(clean, gulp.parallel(styles, images, copy));
+
+export default dev;

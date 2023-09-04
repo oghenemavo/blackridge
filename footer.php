749313fc846f4727389683cea2cee0a30f3d7276
Mavi Onogomuho <princetunes@gmail.com>
2023-09-04 13:49:08 +0100

    setup not so dynamic footer widget

--- footer.php
+++ footer.php
@@ -1,3 +1,47 @@
+        <?php 
+            $footerLayout = '3,3,3,3';
+            $columns = explode(',', $footerLayout);
+            $isWidgetActive = false;
+
+            foreach ($columns as $key => $column) 
+            {
+                $j = $key+1;
+                if (is_active_sidebar('footer-sidebar-' . $j))
+                {
+                    $isWidgetActive = true;
+                }
+            }
+        ?>
+
+        <?php if ($isWidgetActive): ?>
+            <div class="custom-footer">
+                <div class="container">
+                    <div class="row">
+        
+                        <?php foreach($columns as $key => $column): $j = $key+1; ?>
+                            <div class="col-md-<?php echo $column; ?>">
+                                <?php
+                                    if (is_active_sidebar('footer-sidebar-' . $j)) 
+                                    {
+                                        dynamic_sidebar( 'footer-sidebar-' . $j );
+                                    }
+                                ?>
+                            </div>
+                        <?php endforeach; ?>
+                    </div>
+                </div>
+            </div>
+        <?php endif; ?>
+
+        <div class="footer-site-info">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <p class="text-center">All rights reserved</p>
+                    </div>
+                </div>
+            </div>
+        </div>
         <?php wp_footer(); ?>
     </body>
 </html>
--- lib/sidebars.php
+++ lib/sidebars.php
@@ -14,7 +14,27 @@
                     'after_title' => '</h5>',
                 )
             );
-        }
-    }
 
+        }
+        
+    }
+    
+    $footerLayout = '3,3,3,3';
+    $columns = explode(',', $footerLayout);
+
+    foreach ($columns as $key => $columns) {
+        $j = $key + 1;
+
+        register_sidebar( 
+            array(
+                'id' => 'footer-sidebar-' . $j,
+                'name' => sprintf(esc_html__( 'Footer Widget Column %s', 'blackridge' ), $j),
+                'description' => esc_html__( 'Footer widget', 'blackridge' ),
+                'before_widget' => '<section id="%1$s" class="%2$s mb-4">',
+                'after_widget' => '</section>',
+                'before_title' => '<h5>',
+                'after_title' => '</h5>',
+            )
+        );
+    }
     add_action( 'widgets_init', 'blackridge_sidebar_widgets');

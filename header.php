c2619b128e531cae65894933c0bce3d43740e635
Mavi Onogomuho <princetunes@gmail.com>
2023-09-05 15:48:09 +0100

    setup option for logo

--- header.php
+++ header.php
@@ -11,8 +11,11 @@
         <header>
             <nav class="navbar bg-body-tertiary" aria-label="<?php esc_html_e( 'Main Navigation', 'blackridge' ) ?>">
                 <div class="container-fluid">
+                    
                     <a class="navbar-brand" href="<?php echo esc_url( home_url( '/' ) ); ?>">
-                        <?php esc_html( bloginfo( 'name' ) ); ?>
+                        <?php 
+                            has_custom_logo() ? the_custom_logo() : esc_html( bloginfo( 'name' ) );
+                        ?>
                     </a>
 
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
--- lib/theme-support.php
+++ lib/theme-support.php
@@ -6,6 +6,12 @@
         add_theme_support( 'post-thumbnails' );
         add_theme_support( 'html5', array('search-form', 'comment-form', 'comment-list', 'gallery', 'caption') );
         add_theme_support( 'customize-selective-refresh-widgets' );
+        add_theme_support( 'custom-logo',  array(
+            'height' => 200,
+            'width' => 600,
+            'flex-height' => true,
+            'flex-width' => true
+        ));
     }
 
     add_action( 'after_setup_theme', 'blackridge_theme_support' );

bb81754ade239fd4f8fe8e4c4885b197294c3ab4
Mavi Onogomuho <princetunes@gmail.com>
2023-09-06 17:22:32 +0100

    setup display comments

--- lib/enqueue-assets.php
+++ lib/enqueue-assets.php
@@ -8,6 +8,11 @@
         wp_add_inline_style( 'blackridge-stylesheet', $inline_styles );
         
         wp_enqueue_script( 'blackridge-scripts', get_template_directory_uri() . '/dist/assets/js/bundle.js', array('jquery'), '1.0.0', true );
+
+        if (is_singular() && comments_open() && get_option('thread_comments')) 
+        {
+            wp_enqueue_script( 'comment-reply' );
+        }
     }
 
     add_action('wp_enqueue_scripts', 'blackridge_assets');
--- single.php
+++ single.php
@@ -13,28 +13,8 @@
     <div class="container">
         <div class="row">
             <div class="col-md-<?php echo $layout === 'sidebar' ? 8 : 12; ?>">
-                <?php if (have_posts()): ?>
-                    <?php while (have_posts()): ?>
-
-                        <?php the_post(); ?>
-                        
-                        <?php get_template_part( 'template-parts/post/content' ); ?>
-
-                        <?php 
-                            if (get_theme_mod( 'blackridge_display_author_info', true )) 
-                            {
-                                get_template_part( 'template-parts/single/author' ); 
-                            }
-                        ?>
-
-                        <?php get_template_part( 'template-parts/single/navigation' ); ?>
-
-                    <?php endwhile; ?>
-                    
-
-                <?php else: ?>
-                    <?php get_template_part( 'template-parts/post/content', 'none' ); ?>
-                <?php endif; ?>
+                
+                <?php get_template_part( 'loop', 'single' ); ?>
 
             </div>
 

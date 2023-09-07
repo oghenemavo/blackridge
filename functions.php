f34504c49b1268b749666f32f2545ba4aaa35ec5
Mavi Onogomuho <princetunes@gmail.com>
2023-09-07 18:12:06 +0100

    setup first shortcode

--- functions.php
+++ functions.php
@@ -8,3 +8,16 @@
     require_once 'lib/customize.php';
     require_once 'lib/metaboxes.php';
     require_once 'lib/comment-callback.php';
+
+
+    function blackridge_button($atts)
+    {
+        extract(shortcode_atts(array(
+            'color' => 'red',
+            'text' => 'Button',
+        ), $atts));
+
+        return '<button style="background-color: ' . esc_attr($color) . '">' . esc_html($text) . '</button>';
+    }
+
+    add_shortcode('blackridge_button', 'blackridge_button');

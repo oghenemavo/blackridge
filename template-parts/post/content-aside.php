98d6d2a5a690581df042c4c881acf7c6395ee7e7
Mavi Onogomuho <princetunes@gmail.com>
2023-09-12 12:58:43 +0100

    finish up aside post format template

--- template-parts/post/content-aside.php
+++ template-parts/post/content-aside.php
@@ -1,1 +1,17 @@
-aside here *****
+<div class="card my-3">
+
+    <div class="card-body">
+        
+        <div>
+            <?php the_content(); ?>
+        </div>
+
+        <?php if (is_single()): ?>
+            <?php get_template_part('template-parts/post/footer'); ?>
+        <?php endif; ?>
+
+        <h6 class="card-subtitle mb-2 text-body-secondary"><?php echo blackridge_post_short_meta(); ?></h6>
+
+    </div>
+
+</div>
